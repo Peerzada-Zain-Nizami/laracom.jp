788a3fc76db61cd1929e5c60d72662abb9ccb359
Peerzada-Zain-Nizami <qazizainulabaidin@gmail.com>
2024-11-08 18:52:10 +0500

    8-10-2024

---
+++ app/Http/Controllers/BlogPostController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogPostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return BlogPost::with('author')->get();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $blogPost = BlogPost::create($request->all());
+
+        return response()->json($blogPost, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $blogPost = BlogPost::find($id);
+        return response()->json($blogPost->load('author'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required',
+        ]);
+
+        $blogPost = BlogPost::find($id);
+        $blogPost->update($request->all());
+
+        return response()->json($blogPost);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $blogPost = BlogPost::find($id);
+        $blogPost->delete();
+
+        return response()->json(null, 204);
+    }
+
+    public function userBlogPosts(Request $request)
+    {
+        $userId = $request->user()->id; // Get authenticated user ID
+        $blogPosts = BlogPost::where('user_id', $userId)->get();
+
+        return response()->json($blogPosts);
+    }
+}
